b442583bab1f762582d8aabd14b605a369ef2b59
Rex Addiscentis <raddiscentis@addiscent.com>
2016-07-23 03:37:41 +0000

    Use link_to image_tag instead of haml-html on remaining screen shots

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'ec2-52-35-180-65.us-west-2.compute.amazonaws.com', user: 'addiscent', roles: %w{app db web}
+server 'ec2-52-39-251-159.us-west-2.compute.amazonaws.com', user: 'addiscent', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
